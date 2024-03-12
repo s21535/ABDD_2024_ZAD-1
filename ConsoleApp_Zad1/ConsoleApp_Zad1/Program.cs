aa7c16a0a2e1328ceaa7a0885f0f08a828669c87
s21535 <s21535@pjwstk.edu.pl>
2024-03-12 22:25:22 +0100

    feature-average version 1.0

--- ConsoleApp_Zad1/ConsoleApp_Zad1/Program.cs
+++ ConsoleApp_Zad1/ConsoleApp_Zad1/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
+
+static int CalculateAverage(int[] numbers)
+{
+    int sum = 0, average = 0;
+    for (int i = 0; i <= numbers.Length-1; i++)
+    {
+        sum = sum + numbers[i];
+    }
+    average = sum / (numbers.Length-1);
+    return average;
+}
