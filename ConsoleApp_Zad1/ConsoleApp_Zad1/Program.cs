ca18d8281b43f62586629e352fd6edd2a253f823
s21535 <s21535@pjwstk.edu.pl>
2024-03-14 22:05:32 +0100

    FindMaximum method WIP

--- ConsoleApp_Zad1/ConsoleApp_Zad1/Program.cs
+++ ConsoleApp_Zad1/ConsoleApp_Zad1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
@@ -14,4 +16,11 @@ static int CalculateAverage(int[] numbers)
     }
     average = sum / (numbers.Length-1);
     return average;
+}
+
+static int FindMaximum(int[] numbers)
+{
+    int max = 0;
+
+    return max;
 }
